a39407425db305c60198bd94a85435c94fd1aca1
Isabel <yanxiangw04@gmail.com>
2025-03-11 00:03:18 +0800

    Model: Modify Person to support a Remark field

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -16,6 +16,7 @@ public class PersonCard extends UiPart<Region> {
 
     private static final String FXML = "PersonListCard.fxml";
 
+
     /**
      * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
      * As a consequence, UI elements' variable names cannot be set to such keywords
@@ -40,6 +41,9 @@ public class PersonCard extends UiPart<Region> {
     private Label email;
     @FXML
     private FlowPane tags;
+    @FXML
+    private Label remark;
+
 
     /**
      * Creates a {@code PersonCode} with the given {@code Person} and index to display.
